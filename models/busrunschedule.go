e1f3d948d17176fc28125ab04040135b0564e05d
Sujeethan Vigneswaran <sujeethan.vigneswaran@gmail.com>
2023-07-03 22:30:40 -0400

    Fix Bus Run Schedule Model

--- models/busrunschedule.go
+++ models/busrunschedule.go
@@ -6,8 +6,6 @@ import (
 
 type BusRunSchedule struct {
 	BrsId  uint  `json:"brs_id" gorm:"primary_key"`
-	BrID  uint `json:"br_id"`
-	BsId uint `json:"bs_id"`
 	ArrivalTime time.Time `json:"arrival_time"`
 	MinsToArrival uint `json:"mins_to_arrival"`
 }
